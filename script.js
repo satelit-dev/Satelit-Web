436855fc87c12a216867369a529878aeeeef6ba0
satelit-dev <satelit.sakarya@gmail.com>
2025-02-27 11:00:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -79,3 +79,14 @@ document.getElementById('feedbackForm').addEventListener('submit', function(even
     // });
 });
 
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    const scrollPosition = window.scrollY;
+    
+    if (scrollPosition > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
